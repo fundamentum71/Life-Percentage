4a37e144849c0e802257db78f13e3e4417efdcef
Aleksandr <it.kucherov@gmail.com>
2022-07-21 16:33:22 +0300

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,18 @@
 const form = document.querySelector('#form_for_live'),
 	result = document.querySelector('.result');
 
-let title, house, age;
+//let title, house, age;
+
+//console.log(document.querySelector('#age').getAttribute('id'));
+//console.log(document.querySelector('#age').value);
 
 const mainFunction = () => {
-	if (!title || !house) {
+	if (!title || !house || !age) {
 		result.textContent = `____`;
 		return;
 	}
-	console.log(title);
-	console.log(house);
+	//console.log(title);
+	//console.log(house);
 	//console.log(age);
 	const persent = Math.round((100 / +title) * +house);
 	return (result.textContent = `${persent}%`);
@@ -35,7 +38,10 @@ function getDynamicInformation(selector) {
 				house = +input.value;
 				break;
 			case 'age':
-				age = +input.value;
+				age = input.value;
+				const t = Date.parse(new Date()) - Date.parse(age);
+
+				console.log(msInHouse(t));
 				break;
 		}
 		mainFunction();
@@ -44,3 +50,37 @@ function getDynamicInformation(selector) {
 getDynamicInformation('#title');
 getDynamicInformation('#house');
 getDynamicInformation('#age');
+
+const msInHouse = (value) => {
+	const pureHouse = Math.floor(value / (1000 * 60 * 60));
+	return pureHouse;
+};
+
+//*********************************88 */
+//console.log(t);
+//if (t <= 0) {
+//	days = 0;
+//	hours = 0;
+//	minutes = 0;
+//	seconds = 0;
+//} else {
+//	(days = Math.floor(t / (1000 * 60 * 60 * 24))),
+//		(hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
+//		(minutes = Math.floor((t / 1000 / 60) % 60)),
+//		(seconds = Math.floor((t / 1000) % 60));
+//}
+
+//console.log(`Дней${days}`);
+//console.log(`Часов${hours + 3}`);
+////console.log(minutes);
+////console.log();
+
+//return {
+//	total: t,
+//	days,
+//	hours,
+//	minutes,
+//	seconds,
+//};
+
+//*************************** */
