bee374f6c2f0894449966c355fe9e2b04ab08e5e
Aleksandr <it.kucherov@gmail.com>
2022-07-22 12:10:11 +0300

    functional helper

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,77 @@
+//****HELPER */
+
+let yearsToHours, monthsToHours, weeksToHours, daysToHours, sumHourse;
+
+const resultHelper = document.querySelector('.helper__result-allHouse span'),
+	resultHouseInput = document.querySelector('#house');
+
+const resultYearsToHours = document.querySelector('.helper__yearsToHours'),
+	resultMonthsToHours = document.querySelector('.helper__monthsToHours'),
+	resultWeeksToHours = document.querySelector('.helper__weeksToHours'),
+	resultDaysToHours = document.querySelector('.helper__daysToHours');
+
+const funcHelper = () => {
+	if (!yearsToHours || !monthsToHours || !weeksToHours || !daysToHours) {
+		resultHelper.textContent = `Заполните все данные...`;
+		return;
+	}
+
+	sumHourse = yearsToHours + monthsToHours + weeksToHours + daysToHours;
+	resultHouseInput.value = sumHourse;
+
+	return (resultHelper.textContent = `Это в сумме: ${sumHourse} часов`);
+};
+
+funcHelper();
+
+function getDynamicInformationHelper(selector) {
+	const input = document.querySelector(selector);
+
+	input.addEventListener('input', () => {
+		switch (input.getAttribute('id')) {
+			case 'yearsToHours':
+				res = input.value;
+				yearsToHours = funcYearsToHours(res);
+				resultYearsToHours.textContent = `Это ${yearsToHours} часов`;
+				break;
+			case 'monthsToHours':
+				res = +input.value;
+				monthsToHours = funcMonthsToHours(res);
+				resultMonthsToHours.textContent = `Это ${monthsToHours} часов`;
+				break;
+			case 'weeksToHours':
+				res = input.value;
+				weeksToHours = funcWeeksToHours(res);
+				resultWeeksToHours.textContent = `Это ${weeksToHours} часов`;
+				break;
+			case 'daysToHours':
+				res = +input.value;
+				daysToHours = funcDaysToHours(res);
+				resultDaysToHours.textContent = `Это ${daysToHours} часов`;
+				break;
+		}
+		funcHelper();
+	});
+}
+getDynamicInformationHelper('#yearsToHours');
+getDynamicInformationHelper('#monthsToHours');
+getDynamicInformationHelper('#weeksToHours');
+getDynamicInformationHelper('#daysToHours');
+
+const funcYearsToHours = (value) => {
+	return value * 8760;
+};
+const funcMonthsToHours = (value) => {
+	return value * 708;
+};
+const funcWeeksToHours = (value) => {
+	return value * 168;
+};
+const funcDaysToHours = (value) => {
+	return value * 24;
+};
+
+//live calc
 const form = document.querySelector('#form_for_live'),
 	result = document.querySelector('.result');
 
@@ -30,6 +104,10 @@ function getDynamicInformation(selector) {
 				title = input.value;
 				break;
 			case 'house':
+				if (sumHourse) {
+					house = sumHourse;
+					break;
+				}
 				house = +input.value;
 				break;
 			case 'age':
